be6b895a20c3fbfa57774c880e386842b7f2b3f3
Akash Singh <akash142002singh@gmail.com>
2023-02-18 18:25:23 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+const name = "Akash";
+
+function App() {
+  return (
+    <div className="App">
+      Hello World!
+      So Let's Get Started! I am {name}
+    </div>
+  );
+}
+
+export default App;
