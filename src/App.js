076e4b0f33a27a8f0eb890d19341013688294f9a
Akash Singh <akash142002singh@gmail.com>
2023-02-18 20:29:53 +0530

    exercise completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 
 import './App.css';
-
+import { Employ } from './Employ';
 const name = "Akash";
 
 function App() {
   return (
     <div className="App">
-      Hello World!
-      So Let's Get Started! I am {name}
+      <Employ salary ="20000" position ="Junior intern" company ="AR Tech" />
+      <Employ salary ="80000" position ="Junior developer" company ="AR Tech" />
+      <Employ salary ="100000" position ="Senior developer" company ="AR Tech" />
     </div>
   );
 }
